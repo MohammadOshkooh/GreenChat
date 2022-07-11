8154fc13618342cedeb996c3d35b730d5084fe6c
Mohammad Oshkooh <mohammadoshkooh@gmail.com>
2022-07-11 13:45:12 +0430

    update ins=dex.jtml

--- chat/forms.py
+++ chat/forms.py
@@ -7,3 +7,9 @@ class CreateNewGroupForm(forms.ModelForm):
     class Meta:
         model = Chat
         fields = ['room_name']
+        widgets = {
+            'room_name': forms.Textarea(
+                attrs={'id': "chat-message-input", 'name': "", 'class': "form-control type_msg",
+                       'placeholder': "create new group (Enter group name)",
+                       'style': "height: 12px;margin-left: 21px;width: 86%;"})
+        }
