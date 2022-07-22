e742c12891bad11e86342ff0859a7e7e2ff6b342
Mohammad Oshkooh <mohammadoshkooh@gmail.com>
2022-07-22 13:04:25 +0430

    update config

--- accounts/models.py
+++ accounts/models.py
@@ -1,11 +1,12 @@
 from django.contrib.auth import get_user_model
+from django.contrib.auth.models import User
 from django.db import models
 
 
 class Profile(models.Model):
     image = models.ImageField(upload_to='profile/user/%y/%m/%d/', blank=True, null=True,
                               default='http://127.0.0.1:8000/static/img/index.png')
-    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name='profile')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='profile')
 
     def __str__(self):
         return self.user.username
--- config/settings.py
+++ config/settings.py
@@ -232,3 +232,4 @@ CHANNEL_LAYERS = {
         },
     },
 }
+
