732fe53db7d3d224d4c74970fb1e82061be9d5e7
Mohammad Oshkooh <mohammadoshkooh@gmail.com>
2022-08-13 17:41:13 +0430

    fix a bug in ws

--- chat/routing.py
+++ chat/routing.py
@@ -4,6 +4,6 @@ from .consumers import ChatConsumer
 
 websocket_urlpatterns = (
     # re_path(r'ws/chat/(?P<room_name>\w+)/$(?P<link>/w+)/$', ChatConsumer.as_asgi()),
-    path('ws/chat/<room_name>/<link>/', ChatConsumer.as_asgi()),
+    path('wss/chat/<room_name>/<link>/', ChatConsumer.as_asgi()),
     # re_path(r'ws/chat/(?P<room_name>\w+)/$', ChatConsumer.as_asgi()),
 )
